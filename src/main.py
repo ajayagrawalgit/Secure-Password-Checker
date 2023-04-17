4969ee5d40544ce722ae20c231c30464dd55341c
Ajay Agrawal <94609372+ajayagrawalgit@users.noreply.github.com>
2023-04-17 12:48:30 +0530

    change in functions

--- src/main.py
+++ src/main.py
@@ -4,14 +4,6 @@ import sys
 
 def main():
     all_the_passwords = sys.argv[1:]
-    
-    if len(sys.argv) == 1:
-        no_arg_message();
-        sys.exit(0)
-    elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
-        help_message()
-        sys.exit(0)
-
     for passwds in all_the_passwords:
         first_5_chars, rest_all_chars = hash_the_password(passwds)
         pwned_api_response = pwned_api_data(first_5_chars)
@@ -26,4 +18,10 @@ def main():
 ####################################################################################################
 
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        no_arg_message();
+        sys.exit(0)
+    elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        help_message()
+        sys.exit(0)
    main()
