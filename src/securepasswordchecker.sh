c6e32646802f3c5103ceb3113321a87f3452e73d
Ajay Agrawal <94609372+ajayagrawalgit@users.noreply.github.com>
2023-04-17 12:55:48 +0530

    added the loop instead of all in one variable

--- src/securepasswordchecker.sh
+++ src/securepasswordchecker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-all_the_passwords=("$@")
-
-python3 /usr/local/lib/Secure-Password-Checker/src/main.py $all_the_passwords
+for arg in "$@"
+do
+  python3 /usr/local/lib/Secure-Password-Checker/src/main.py $arg
+done
